878978f8a505aa9832b9ba6a69dca0cc59e8af3e
Luke <luke@easychat.co>
2021-12-04 17:14:38 +0800

    Modify part 1 solution with a  block

--- day1/part1.py
+++ day1/part1.py
@@ -1,11 +1,10 @@
-f = open("./part1-input.txt", "r")
-increasementCount = 0
-first = f.readline()
-second = f.readline()
-while first and second:
-    if int(second) - int(first) > 0:
-        increasementCount += 1
-    first = second
+with open("./part1-input.txt", "r") as f:
+    increasementCount = 0
+    first = f.readline()
     second = f.readline()
-print(increasementCount)
-f.close()
+    while first and second:
+        if int(second) - int(first) > 0:
+            increasementCount += 1
+        first = second
+        second = f.readline()
+    print(increasementCount)
