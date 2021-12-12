af2b2af2c0fb955a24d2cbfd441424d32daf34f5
Luke <luke@easychat.co>
2021-12-12 23:08:57 +0800

    Add the part 2's solution of day 7

---
+++ day7/part2.py
@@ -0,0 +1,15 @@
+import sys
+
+with open("./day7-input.txt", "r") as f:
+    crabs = [int(c) for c in f.readline().split(",")]
+    max_position = max(crabs)
+    min_position = min(crabs)
+    minimum_cost = sys.maxsize
+    for i in range(min_position, max_position):
+        cost = 0
+        for crab in crabs:
+            c = (1 + abs(crab - i)) * (abs(crab - i)) / 2
+            cost += c
+        minimum_cost = min(minimum_cost, cost)
+
+    print(minimum_cost)
