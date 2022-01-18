699c1ee04bfdee85d7ed4ed1b48c6c8aeafc4d6c
Luke <luke@easychat.co>
2022-01-18 11:27:18 +0800

    Add the part 1's solution in day 9

---
+++ day9/part1.py
@@ -0,0 +1,17 @@
+with open('./day9-input.txt', 'r') as f:
+    heightmap = [list(map(lambda n: int(n), list(row.replace('\n', '')))) for row in f.readlines()]
+    risk_level = 0
+    for i, locations in enumerate(heightmap):
+        for j, height in enumerate(locations):
+            if j + 1 < len(locations) and locations[j + 1] <= height:
+                continue
+            if j - 1 >= 0 and locations[j - 1] <= height:
+                continue
+            if i - 1 >= 0 and heightmap[i - 1][j] <= height:
+                continue
+            if i + 1 < len(heightmap) and heightmap[i + 1][j] <= height:
+                continue
+
+            risk_level = risk_level + height + 1
+
+    print(risk_level)
